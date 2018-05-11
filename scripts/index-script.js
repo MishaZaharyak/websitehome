266154e9acdb30b578fae814892199780ad93282
Myhaylo Zaharyak <mishazaharyak@gmail.com>
2018-05-11 12:03:19 +0300

    commit

---
+++ scripts/index-script.js
@@ -0,0 +1,20 @@
+// const anchor = document.getElementsByClassName('anchor')[0];
+// const menu = document.getElementsByClassName('menu')[0];
+// const list = document.getElementsByClassName('menu-list')[0];
+// const items = document.getElementsByClassName('list-item');
+
+// anchor.addEventListener('click', function (el) {
+// 	const name = el.target.className;
+// 	if (name === 'anchor') {
+// 		menu.className += " a";
+// 	    list.className += " b";
+// 		for (let i =0; i <= items.length -1; i++) {
+// 			items[i].className += " c";
+// 			console.log(items[i].className);
+// 		}
+// 	} else {
+		
+// 	}
+	
+// });
+
