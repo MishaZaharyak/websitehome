9dbf253c47daeecf8c8941688a906f812864f70d
Myhaylo Zaharyak <mishazaharyak@gmail.com>
2018-05-13 00:29:58 +0300

    modification

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,15 +1,18 @@
 const anchor = document.getElementsByClassName('anchor')[0];
 const menu = document.getElementsByClassName('menu')[0];
-const list = document.getElementsByClassName('menu-list')[0];
-const items = document.getElementsByClassName('list-item');
-const itemsAnchor = document.querySelectorAll('.list-item a');
-console.log(itemsAnchor);
 let opened = false;
 
 anchor.addEventListener('click', function (el) {
 	const name = el.target.className;
 	opened = !opened;
-	opened ? menu.className += " a" : menu.className = name;
-	
+	opened ? menu.className += " dropMenu" : menu.classList.remove("dropMenu");
 });
 
+const listItemAnchor = document.querySelectorAll('.menu .menu-list .list-item a');
+listItemAnchor.forEach(function(anchor) {
+	anchor.addEventListener('click', function () {
+		menu.classList.remove("dropMenu");
+		opened = !opened;
+	})
+})
+console.log(listItemAnchor);
